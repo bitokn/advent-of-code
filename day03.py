f337f599467e3997688ed30ac1acb44a6097edf5
bernabe <b.kondrak45@gmail.com>
2024-12-03 00:55:09 -0700

    formatted day03

--- day03.py
+++ day03.py
@@ -3,18 +3,7 @@ def main():
         line = file.read()
         print(line)
         sum = 0
-        str_nums = [
-            "0",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-        ]
+        str_nums = ["0","1","2","3","4","5","6","7","8","9"]
         switch_on = True
 
         for i in range(len(line)):
@@ -34,16 +23,13 @@ def main():
                         not_comma = 0
                     else:
                         string = string + line[i + j + 4]
-
                     j += 1
 
                 print("string1", int(string))
-
                 k = 0
                 string2 = "0"
                 not_endparen = 1
                 while not_endparen:
-
                     if line[i + j + k + 4] == ")":
                         not_endparen = 0
                     elif line[i + k + j + 4] not in str_nums:
@@ -51,24 +37,20 @@ def main():
                         string2 = "0"
                         cancel = True
                         not_endparen = 0
-
                     else:
                         string2 = string2 + line[i + j + k + 4]
-
                     k += 1
 
                 if not cancel and switch_on:
                     print("string2", int(string2))
-
                     product = int(string) * int(string2)
                     print(f"{string}*{string2} = {product}")
-
                     sum += product
+
             elif line[i : i + 4] == "do()":
                 switch_on = True
             elif line[i : i + 7] == "don't()":
                 switch_on = False
-                # print(string2)
         print(sum)
 
 
